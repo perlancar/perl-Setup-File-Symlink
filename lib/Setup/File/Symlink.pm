0a87c57a52b7e4528c8520b4d9cfe192602dc082
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-15 18:19:23 +0700

    WIP (convert rmsym and ln_s from step into function)

--- lib/Setup/File/Symlink.pm
+++ lib/Setup/File/Symlink.pm
@@ -8,6 +8,7 @@ use Log::Any '$log';
 use File::Copy::Recursive qw(rmove);
 use File::Path qw(remove_tree);
 use Perinci::Sub::Gen::Undoable 0.09 qw(gen_undoable_func);
+use Setup::File;
 use UUID::Random;
 
 require Exporter;
@@ -20,137 +21,117 @@ our %SPEC;
 
 my $res;
 
-# move mv to Setup::File
-
 $res = gen_undoable_func(
     v           => 2,
-    name        => 'rm_r',
-    summary     => 'Delete file/dir',
-    trash_dir   => 1,
+    name        => 'rmsym',
+    summary     => 'Delete symlink',
     description => <<'_',
 
-It actually moves the file/dir to a unique name in trash and save the unique
-name as undo data.
+Will not delete non-symlinks.
+
+Fixed state: `path` doesn't exist.
+
+Fixable state: `path` exists, is a symlink, (and if `target` is defined, points
+to `target`).
 
 _
     args        => {
         path => {
             schema => 'str*',
         },
+        target => {
+            summary => 'Only delete if existing symlink has this target',
+            schema => 'str*',
+        },
+    },
+    check_args => sub {
+        # TMP, schema
+        my $args = shift;
+        defined($args->{path}) or return [400, "Please specify path"];
+        [200, "OK"];
     },
     check_or_fix_state => sub {
         my ($which, $args, $undo) = @_;
 
-        my $path = $args->{path};
-        my $exists = (-l $path) || (-e _);
-        my $save = "$args->{-undo_trash_dir}/". UUID::Random::generate;
+        my $path     = $args->{path};
+        my $target   = $args->{target};
+        my $is_sym   = (-l $path);
+        my $exists   = $is_sym || (-e _);
+        my $curtarget; $curtarget = readlink($path) if $is_sym;
         my @u;
         if ($which eq 'check') {
-            if ($exists) {
-                push @u, [__PACKAGE__.'mv', {
-                    from => $save,
-                    to   => $path,
-                }];
-            }
-            return @u ? [200, "OK", \@u] : [304, "Already removed"];
+            return [412, "Not a symlink"] if $exists && !$is_sym;
+            return [412, "Target does not match ($curtarget)"] if $is_sym &&
+                defined($target) && $curtarget ne $target;
+            push @u, [__PACKAGE__.'::ln_s', {
+                symlink => $path,
+                target  => $target // $curtarget,
+            }] if $exists;
+            return @u ? [200,"OK",undef,{undo_data=>\@u}]:[304,"Nothing to do"];
         }
-        if (rmove $path, $save) {
+        if (unlink $path) {
             return [200, "OK"];
         } else {
-            return [500, "Can't move $path -> $save: $!"];
+            return [500, "Can't remove symlink: $!"];
         }
     },
 );
-die "Can't generate rm_r: $res->[0] - $res->[1]" unless $res->[0] == 200;
+die "Can't generate rmsym: $res->[0] - $res->[1]" unless $res->[0] == 200;
+
+$res = gen_undoable_func(
+    v           => 2,
+    name        => 'ln_s',
+    summary     => 'Create symlink',
+    description => <<'_',
+
+Fixed state: `symlink` exists and points to `target`.
+
+Fixable state: `symlink` doesn't exist.
+
+_
+    args        => {
+        symlink => {
+            summary => 'Path to symlink',
+            schema => 'str*',
+        },
+        target => {
+            summary => 'Path to target',
+            schema => 'str*',
+        },
+    },
+    check_args => sub {
+        # TMP, schema
+        my $args = shift;
+        defined($args->{symlink}) or return [400, "Please specify symlink"];
+        [200, "OK"];
+    },
+    check_or_fix_state => sub {
+        my ($which, $args, $undo) = @_;
+
+        my $symlink  = $args->{symlink};
+        my $target   = $args->{target};
+        my $is_sym   = (-l $symlink);
+        my $exists   = $is_sym || (-e _);
+        my $curtarget; $curtarget = readlink($symlink) if $is_sym;
+        my @u;
+        if ($which eq 'check') {
+            return [412, "Path already exists"] if $exists && !$is_sym;
+            return [412, "Symlink points to another target"] if $is_sym &&
+                $curtarget ne $target;
+            push @u, [__PACKAGE__.'::rmsym', {path => $symlink}] if !$exists;
+            return @u ? [200,"OK",undef,{undo_data=>\@u}]:[304,"Nothing to do"];
+        }
+        if (symlink $target, $symlink) {
+            return [200, "OK"];
+        } else {
+            return [500, "Can't symlink: $!"];
+        }
+    },
+);
+die "Can't generate ln_s: $res->[0] - $res->[1]" unless $res->[0] == 200;
 
 1;
 __END__
-
-        restore => {
-            summary => 'Restore file/dir previously deleted by rm_r',
-            description => <<'_',
-Rename back file/dir in the trash to the original path.
-_
-            check => sub {
-                return [200, "OK", ["rm_r"]];
-            },
-            fix => sub {
-                my ($args, $step, $undo) = @_;
-                my $f  = $args->{symlink};
-                if ((-l $f) || (-e _)) {
-                    return [412, "Can't restore $step->[1]: $f exists"];
-                } elsif (!(rmove $step->[1], $f)) {
-                    return [500, "Can't restore $step->[1] -> $f: $!"];
-                }
-            },
-        },
-        rmsym => {
-            summary => "Delete symlink at 'path'",
-            description => <<'_',
-
-Syntax: `["rmsym"]`
-
-Will also delete 'path' even if it is a file (not a symlink). Will fail if
-'path' is a directory. The original symlink target is saved as undo data.
-
-See also: ln.
-
-_
-            check => sub {
-                my ($args, $step) = @_;
-                my $s = $args->{symlink};
-                if ((-l $s) || (-e _)) {
-                    my $t = readlink($s) // "";
-                    return [200, "OK", ["ln", $t]];
-                }
-                return [200, "OK"];
-            },
-            fix => sub {
-                my ($args, $step, $undo) = @_;
-                my $s = $args->{symlink};
-
-                if (unlink $s) {
-                    return [200, "OK"];
-                } else {
-                    return [500, "Can't remove $s: $!"];
-                }
-            },
-        },
-        ln => {
-            summary => 'Create symlink',
-            description => <<'_',
-
-Syntax: `["ln", $t?]`.
-
-Create symlink which points to $t, or by default to the 'target' argument.
-
-See also: rmsym.
-
-_
-            check => sub {
-                my ($args, $step) = @_;
-                my $s = $args->{symlink};
-                my $t = $step->[1] // $args->{target};
-                unless ((-l $s) && readlink($s) eq $t) {
-                    return [200, "OK", ["rmsym"]];
-                }
-                return [200, "OK"];
-            },
-            fix => sub {
-                my ($args, $step, $undo) = @_;
-                my $s = $args->{symlink};
-                my $t = $step->[1] // $args->{target};
-                if (symlink $t, $s) {
-                    return [200, "OK"];
-                } else {
-                    return [500, "Can't symlink $s -> $t: $!"];
-                }
-            },
-        },
-    },
-);
-
-);
 
 $res = gen_undoable_func(
     v => 2,
@@ -245,10 +226,10 @@ _
         my $symlink    = $args->{symlink};
         my $target     = $args->{target};
 
-        my $is_symlink = (-l $symlink); # -l performs lstat()
+        my $is_sym     = (-l $sym); # -l performs lstat()
         my $exists     = (-e _);        # now we can use -e
         my $is_dir     = (-d _);
-        my $cur_target = $is_symlink ? readlink($symlink) : "";
+        my $cur_target = $is_sym ? readlink($symlink) : "";
 
         my @steps;
         if ($exists && !$is_symlink) {
---
+++ t/ln_s.t
@@ -0,0 +1,104 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+use Test::More 0.96;
+
+use File::chdir;
+use File::Slurp;
+use File::Temp qw(tempdir);
+use Setup::File::Symlink;
+use Test::Setup qw(test_setup);
+
+plan skip_all => "symlink() not available"
+    unless eval { symlink "", ""; 1 };
+
+my $rootdir = tempdir(CLEANUP=>1);
+$CWD = $rootdir;
+
+test_ln_s(
+    name          => "ln",
+    symlink       => "/s",
+    target        => "/t",
+    check_unsetup => {exists=>0},
+    check_setup   => {},
+    cleanup       => sub { unlink "s" },
+);
+
+write_file("$rootdir/s", "");
+test_ln_s(
+    name          => "unfixable: s already exists (file)",
+    symlink       => "/s",
+    target        => "/t",
+    check_unsetup => {exists=>1, is_symlink=>0},
+    check_setup   => {},
+    dry_do_error  => 412,
+    cleanup       => sub { unlink "s" },
+);
+
+DONE_TESTING:
+done_testing();
+if (Test::More->builder->is_passing) {
+    #diag "all tests successful, deleting test data dir";
+    $CWD = "/";
+} else {
+    diag "there are failing tests, not deleting test data dir $rootdir";
+}
+
+sub test_ln_s {
+    my (%targs) = @_;
+
+    my %tsargs;
+
+    for (qw/name dry_do_error do_error set_state1 set_state2 prepare cleanup/) {
+        $tsargs{$_} = $targs{$_};
+    }
+    $tsargs{function} = \&Setup::File::Symlink::ln_s;
+
+    my $symlink = $rootdir . $targs{symlink};
+    my $target  = $rootdir . $targs{target};
+    my %fargs = (symlink => $symlink, target => $target,
+                 -undo_trash_dir=>$rootdir, %{$targs{other_args} // {}},
+             );
+    $tsargs{args} = \%fargs;
+
+    my $check = sub {
+        my %cargs = @_;
+
+        my $is_symlink = (-l $symlink);
+        my $exists     = (-e _);
+        my $cur_target = $is_symlink ? readlink($symlink) : "";
+
+        my $te = $cargs{exists} // 1;
+        if ($te) {
+            ok($exists, "exists");
+            if ($cargs{is_symlink} // 1) {
+                ok($is_symlink, "is symlink");
+                if (defined $cargs{target}) {
+                    is($cur_target, $cargs{target}, "target is $cargs{target}");
+                } elsif ($cargs{test_target} // 1) {
+                    is($cur_target, $target, "target is $target");
+                }
+            } else {
+                ok(!$is_symlink, "not symlink");
+            }
+        } else {
+            ok(!$exists, "does not exist");
+        }
+        if ($cargs{extra}) {
+            $cargs{extra}->();
+        }
+    };
+
+    $tsargs{check_setup}   = sub { $check->(%{$targs{check_setup}}) };
+    $tsargs{check_unsetup} = sub { $check->(%{$targs{check_unsetup}}) };
+    if ($targs{check_state1}) {
+        $tsargs{check_state1} = sub { $check->(%{$targs{check_state1}}) };
+    }
+    if ($targs{check_state2}) {
+        $tsargs{check_state2} = sub { $check->(%{$targs{check_state2}}) };
+    }
+
+    test_setup(%tsargs);
+}
